e6c136089dee5ec2289e9cf3fc073f33b5869d5d
rootm0s <rickardmalmkvist@hotmail.com>
2017-10-02 18:56:11 +0200

    Custom User-Agents for testing the proxies
    Custom User-Agents for testing the proxies

--- pull-and-test-proxies.py
+++ pull-and-test-proxies.py
@@ -1,131 +1,103 @@
 import requests
 import time
 import os
-import thread
+import threading
 import sys
+import logging
+import random 
 
-urls = ["http://pubproxy.com/api/proxy?format=txt&type=http&limit=20&user_agent=true"]		
+urls = ["http://pubproxy.com/api/proxy?format=txt&type=http&limit=20&level=elite&user_agent=true"]
+site = "http://httpbin.org/get"
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(levelname)s] (%(threadName)-10s) %(message)s',)
+				
 def banner():
 	os.system("cls" if os.name == "nt" else "clear")
 	
-	print '\033[36m' +'''
+	print '\033[36m' + '''
     ___o .--.
    /___| |OO|
-  /'   |_|  |_    - Downloads proxies via open api and remove duplicates.
-       (_    _)     Tests each proxy, saves the working ones in a text file
-       | |   \      
-       | |__./
+  /'   |_|  |_    - Downloads proxies from URL and tests each proxie
+       (_    _)     
+       | |  | 
+       | |__|
 	''' + '\033[37m'
 
-def systime():
+def proxie_list_load():
 	'''
-	Grab systemtime
+	Load proxie list from file
 	'''
-	return time.strftime("%H:%M:%S")
-
-def clean():
-	'''
-	Attempt to remove the tmp files
-	'''
-	if os.path.isfile("http.tmp"):
-		os.remove("http.tmp")
-	else:
-		pass
-	if os.path.isfile("http_non_dupes.tmp"):
-		os.remove("http_non_dupes.tmp")
-	else:
-		pass
-
-def proxie_sort():
-	'''
-	Sort the list and remove duplicate proxies
-	'''
-	print "[{}] Checking for duplicates".format(systime())
-	content = open("http.tmp","r").readlines()
-	content_set = set(content)
+	proxylist = open("result.txt").readlines()
+	proxylist = [x.strip() for x in proxylist]
 	
-	cleandata = open("http_non_dupes.tmp","w")
-	for line in content_set:
-		cleandata.write(line)
-	print "[{}] Duplicates removed".format(systime())
+	return proxylist
+
+def random_useragent():
+	'''
+	Random User-Agent for our HTTP requests
+	'''
+	user_agents = ['Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36', 
+				'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
+				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A',
+				'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+				'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.2 (KHTML, like Gecko) ChromePlus/4.0.222.3 Chrome/4.0.222.3 Safari/532.2',
+				'Mozilla/5.0 (Windows; U; Win 9x 4.90; SG; rv:1.9.2.4) Gecko/20101104 Netscape/9.1.0285',
+				'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
+				'Opera/12.02 (Android 4.1; Linux; Opera Mobi/ADR-1111101157; U; en-US) Presto/2.9.201 Version/12.02',
+				'Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (J2ME/23.377; U; en) Presto/2.5.25 Version/10.54',
+				'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.1.0.346 Mobile Safari/534.11+',
+				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246']
+
+	user_agent = random.choice(user_agents)
+	user_agent_set = {'User-Agent' : '{}'.format(user_agent)}
+	
+	return user_agent_set
 
 def proxie_pull():
 	'''
-	Pull proxies via api
+	Download proxie list from url and save results
 	'''
-	print "[{}] Pulling proxies".format(systime())
 	for _ in range(1):
 		for url in urls:
 			try:
-				r = requests.get(url)
-				if (r.status_code == requests.codes.ok):
-					print "[{}] API call ({})".format(systime(),'\033[33m' + url + '\033[37m')
-					with open('http.tmp', 'a') as f:
-						f.write(r.content + "\n")
+				request = requests.get(url,headers=random_useragent(),timeout=7.0)
+				if (request.status_code == requests.codes.ok):
+					with open("result.txt", 'a') as f:
+						f.write(request.content + "\n")
 						f.close()
-				elif (r.status_code != 200):
-					print "[{}] Error".format(systime())
+				elif (request.status_code != 200):
+					pass
 			except Exception as e:
-				print "[{}] Error".format(systime())
+				pass
 			except KeyboardInterrupt:
-				print "[{}] Quiting".format(systime())
 				sys.exit()
 
 def proxie_tests():
 	'''
-	Testing all the proxies found in the sorted list
-	Auto timeout after 7 seconds.
+	Load proxies and test them against url using fake User-Agents
 	'''
-	
-	proxylist = open("http_non_dupes.tmp").readlines()
-	proxylist = [x.strip() for x in proxylist]
-
-	def run():
-		for proxy in proxylist:
-			sites = "http://google.com","http://facebook.com"
-			proxies = {"http": "{}".format(proxy),"https": "{}".format(proxy)}
-			for site in sites:
-				try:
-					r = requests.get(site,proxies=proxies,timeout=7.0)
-					if (r.status_code == requests.codes.ok):
-						print "[{}] Testing Proxy: {} against ({})".format(systime(),
-							'\033[32m' + proxy + '\033[37m',site)
-						with open('result.txt', 'a') as f:
-							f.write(proxy + "\n")
-							f.close()
-					elif (r.status_code != 200):
-						# Print yellow text if response is bad
-						print "[{}] Testing Proxy: {} against ({})".format(systime(),
-							'\033[33m'+ proxy + '\033[37m',site)
-				except Exception as E:
-					# Print red text if response is none
-					print "[{}] Testing Proxy: {} against ({})".format(systime(),
-						'\033[31m'+ proxy + '\033[37m',site)
-				except KeyboardInterrupt:
-					print "[{}] Quiting".format(systime())
-					sys.exit()
-
-	'''
-	Start two threads
-	'''
-	try:
-		thread.start_new_thread(run(),())
-		thread.start_new_thread(run(),())
-	except TypeError:
-		pass
+	for proxy in proxie_list_load():
+		proxies = {"http": "{}".format(proxy), "https": "{}".format(proxy)}
+		try:
+			request = requests.get(site,headers=random_useragent(),proxies=proxies,timeout=7.0)
+			if (request.status_code == requests.codes.ok):
+				with open('result-working.txt', 'a') as f:
+					f.write(proxy + "\n")
+					f.close()
+			elif (request.status_code != 200):
+				pass
+		except Exception as E:
+			pass
+		except KeyboardInterrupt:
+			sys.exit()
 
 def main():
-	'''
-	Load all functions
-	'''
 	banner()
-	clean() # Clean at start
 	proxie_pull()
-	proxie_sort()
-	proxie_tests()
-	clean() # Clean once done
-	print "[{}] Finished".format(systime())
+	
+	thread = threading.Thread(target=proxie_tests)
+	thread.start()
 
 if __name__ == "__main__":
 	main()
